695f0aa4313d45b11d98fbe30b8040a69a5cc987
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-03-12 01:23:35 -0400

    Added target-cpu=native to RUSTFLAGS in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 ./target/release/finn-assembler "$(< ${1})" > output/src/main.rs
 
 cd output
-cargo build --release
-# cargo rustc --release -- -C target-cpu=native
+# cargo build --release
+RUSTFLAGS="-C target-cpu=native" cargo rustc --release -- -C target-cpu=native
 time ./target/release/output
 cd ..
